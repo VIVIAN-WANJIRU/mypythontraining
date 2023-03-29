9addb1d58613b0b753e170202ec52726e146f2e2
Vivian <viviankariuki028@gmail.com>
2023-03-29 16:43:29 +0300

    first commit

---
+++ task2.py
@@ -0,0 +1,19 @@
+number = float(input("Enter the number:"))
+if (number%2 == 0):
+    print("even")
+    if (number%4 == 0):
+        print("Multiple of 4")
+else:
+    print("odd")        
+
+
+
+
+
+
+
+
+
+
+
+
