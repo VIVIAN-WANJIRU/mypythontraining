d20ed2e8dfea0de95377c75f035cce210e29c3c0
Vivian <viviankariuki028@gmail.com>
2023-04-19 15:38:33 +0300

    first commit

---
+++ cubetask.py
@@ -0,0 +1,13 @@
+l=float(input("enter the l:"))
+def cube(l):
+    surface_area=6*(l**2)
+    return surface_area
+
+area=cube(l)
+print("Surface_area is:",area)
+
+def cube(l):
+    volume=(l**3)
+    return volume
+volume=cube(l)
+print("Volume is:",volume)    
